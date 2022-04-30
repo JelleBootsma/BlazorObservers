68271fb4fdf831680e25c96f4aa8f314710a0ec6
Jelle Bootsma <jelle@authore.com>
2022-04-30 13:06:26 +0200

    Repo restructure

---
+++ ObserverLibrary/DI/DependencyInjectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using BlazorObservers.ObserverLibrary.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorObservers.ObserverLibrary.DI
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddResizeObserverRegistrationService(this IServiceCollection services)
+        {
+            return services.AddScoped<ResizeObserverRegistrationService>();
+        }
+    }
+}
